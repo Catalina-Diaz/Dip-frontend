60bbb6ee6370e2e71aa2d2d7babc171cbf432420
Andree-Zzz <mad08061@gmail.com>
2020-12-13 16:03:53 -0500

    Pasando Home

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <!-- Contenedor principal -->
   <div class="w-100">
     <!-- Barra de navegacion -->
-    <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #6B4BF9;">
+    <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #640ABA;">
       <a class="navbar-brand" @click="reset()" href="#">
         <img src="@/assets/iconos/Gallery.png" width="35" height="35" class="d-inline-block align-top" alt="icono" loading="lazy" draggable="false">
         <span class="ml-1 h4">Catalogos en linea</span>
@@ -37,30 +37,6 @@
       </div>
     </nav>
     <!-- End, Barra de navegacion -->
-
-    <!-- contenedor Usuarios -->
-    <div v-if="userData">
-      <h3 class="ml-4 mt-3">Establecimientos:</h3>
-      <!-- Usuarios -->
-      <div class="row m-2">
-        <div @click="next(user.id)" class="col-sm-6 col-md-3" v-for="(user, id) in usuarios" :key="id">
-          <div class="card mb-3" style="">
-            <img :src="'http://localhost:1337'+user.imagen_usuario.imagen.url" class="card-img-top" alt="">
-            <div class="card-body px-2 pt-2 pb-1">
-              <h5 class="card-title">{{user.username}}</h5>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- End, Usuarios -->
-    </div>
-    <!-- End, contenedor Usuarios -->
-
-    <!-- sin usuarios -->
-    <div v-if="userVacios">
-      <h3 class="ml-4 mt-3">Sin establecimientos registrados...</h3>
-    </div>
-    <!-- End, sin usuarios -->
 
   </div>
   <!-- End, Contenedor principal -->
@@ -263,9 +239,6 @@
   export default {
     data() {
       return {
-        usuarios: [],
-        userVacios: false,
-        userData: false,
         username: '',
         email: '',
         telefono: '',
@@ -287,30 +260,8 @@
       if(localStorage.getItem('iflogin')){
         this.ifsalir = true;
       }
-      this.traerUsuarios(); 
     },
     methods: {
-      traerUsuarios() {
-        axios.get("http://localhost:1337/users", {
-          params: {
-            confirmed: true,
-          }}).then((response) => {
-            this.usuarios = response.data;
-            if(this.usuarios) {
-              this.userVacios = false;
-              this.userData = true;
-            } else {
-              this.userVacios = true;
-              this.userData = false;
-            }
-            // this.usuarios.forEach((user) => {
-            //   console.log(user.username)
-            // })
-        }).catch((error) => {
-          this.userVacios = true;
-          this.userData = false;
-        });
-      },
       registro() {
         axios.post("http://localhost:1337/auth/local/register", {
           username: this.username,
@@ -336,7 +287,8 @@
               password: this.l_password,
         }).then((response) =>{
           console.log('login');
-         localStorage.setItem('iflogin', false);
+          localStorage.setItem('iflogin', false);
+          localStorage.setItem('ifregistro', false);
           localStorage.setItem('ifsalir', true);
           localStorage.setItem('token', response.data.jwt);
           location.reload();
@@ -345,12 +297,8 @@
       cambiarImagen() {
         this.userImagen = event.target.files[0];
       },
-      next(userId) {
-        localStorage.setItem('userId', userId);
-        console.log(userId);
-      },
       salir() {
-      localStorage.removeItem('token');
+        localStorage.removeItem('token');
         localStorage.removeItem('iflogin');
         localStorage.removeItem('ifsalir');
         localStorage.removeItem('ifregistro');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,78 @@
 <template>
+  <div>
+    <!-- contenedor Usuarios -->
+    <div v-if="userData">
+      <h3 class="ml-4 my-3">Establecimientos:</h3>
+      <!-- Usuarios -->
+      <div class="row m-2">
+        <div @click="next(user.id)" class="col-sm-6 col-md-3" v-for="(user, id) in usuarios" :key="id">
+          <div class="card mb-3" style="">
+            <img :src="'http://localhost:1337'+user.imagen_usuario.imagen.url" class="card-img-top" alt="">
+            <div class="card-body px-2 pt-2 pb-1">
+              <h5 class="card-title">{{user.username}}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End, Usuarios -->
+    </div>
+    <!-- End, contenedor Usuarios -->
 
+    <!-- sin usuarios -->
+    <div v-if="userVacios">
+      <h3 class="ml-4 mt-3">Sin establecimientos registrados...</h3>
+    </div>
+    <!-- End, sin usuarios -->
+  </div>
 </template>
 
 <script>
+  import axios from "axios";
   export default {
     name: 'Home',
+    data() {
+      return {
+        usuarios: [],
+        userVacios: false,
+        userData: false,
+      }
+    },
+    mounted() {
+      if(localStorage.getItem('ifregistro')){
+        this.ifregistro = false;
+      }
+      if(localStorage.getItem('iflogin')){
+        this.iflogin = false;
+      }
+      if(localStorage.getItem('iflogin')){
+        this.ifsalir = true;
+      }
+      this.traerUsuarios(); 
+    },
+    methods: {
+      traerUsuarios() {
+        axios.get("http://localhost:1337/users", {
+          params: {
+            confirmed: true,
+          }}).then((response) => {
+            this.usuarios = response.data;
+            if(this.usuarios) {
+              this.userVacios = false;
+              this.userData = true;
+            } else {
+              this.userVacios = true;
+              this.userData = false;
+            }
+        }).catch((error) => {
+          this.userVacios = true;
+          this.userData = false;
+        });
+      },
+      next(userId) {
+        localStorage.setItem('userId', userId);
+        console.log(userId);
+      },
+    }
   }
 </script>
 
